b787fd0cd55564474c194a57a333e0ed30eaa9ca
Ron Avnery <avnery@gmail.com>
2019-07-12 23:30:57 +0300

    login,register header design

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -2,12 +2,17 @@
   <section class="app-header">
     <div class="logo-container">
       <img class="logo" src="../assets/logo.png" />
+      <span class="logo-title fs14">LevelUp</span>
     </div>
     <div class="nav-container fs14">
-    <router-link exact to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/explore">Explore</router-link>
-    <router-link to="/sign-in">Sign-in</router-link>
+      <router-link exact to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/explore">Explore</router-link>
+      <router-link to="/sign-in">Sign-in</router-link>
+    </div>
+    <div class="user-action-container">
+      <button class="btn-login">Login</button>
+      <button class="btn-register">Register</button>
     </div>
   </section>
 </template>
@@ -17,24 +22,41 @@ export default {};
 </script>
 
 <style scoped lang="scss">
-a {
-  padding: rem(10px);
-}
-
 .app-header {
   @include flexCustom(space-between, center, row);
   background: $tpPurple;
   height: rem(50px);
-  text-transform: uppercase;
-  font-family: 'Montserrat', sans-serif;
+  font-family: $Montserrat-Regular;
+  padding: 0 rem(100px) 0 rem(100px);
+}
+
+a {
+  padding: rem(10px);
+}
+
+.logo-container {
+  @include flexCenter(center);
+}
+
+.logo-title {
+  margin-left: rem(5px);
 }
 
 .logo {
   width: rem(30px);
-  margin-left: rem(40px);
 }
 
 .nav-container {
-  margin-right: rem(40px)
+  margin-right: rem(40px);
+  text-transform: uppercase;
+}
+
+.btn-register {
+  @include btnActionColorSm;
+}
+
+.btn-login {
+  @include btnActionCleanSm;
+  margin-right: rem(10px)
 }
 </style>

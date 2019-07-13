b1799f0cf12c26be63e99db24c9240664e81b528
Ron Avnery <avnery@gmail.com>
2019-07-13 17:38:42 +0300

    minor changes

--- frontend/src/components/OfferDetails.vue
+++ frontend/src/components/OfferDetails.vue
@@ -1,15 +1,25 @@
 <template>
   <section class="offer-details-container">
-    <div class="offer-details-card">
-      <img :src="offer.imgs[0]" /><br>
-      <span>{{offer.title}}</span><br>
-      <span>{{offer.description}}</span><br>
-      Requirements:
-      <span v-for="requirement in offer.requirements" :key="requirement">{{requirement}}</span><br>
-      <span>Stars:{{offer.stars}}</span><br>
-      Meeting Type: <span>{{offer.location.type}}</span><br>
-      Address: <span>{{offer.location.address}}</span><br>
-      Reviews: <span v-for="review in offer.reviews" :key="review">{{review}}</span><br>
+    <div class="offer-details-card" v-if="offer">
+      <img :src="offer.imgs[0]" />
+      <br />
+      <span>{{offer.title}}</span>
+      <br />
+      <span>{{offer.description}}</span>
+      <br />Requirements:
+      <span
+        v-for="requirement in offer.requirements"
+        :key="requirement"
+      >{{requirement}}</span>
+      <br />
+      <span>Stars:{{offer.stars}}</span>
+      <br />Meeting Type:
+      <span>{{offer.location.type}}</span>
+      <br />Address:
+      <span>{{offer.location.address}}</span>
+      <br />Reviews:
+      <span v-for="(review,idx) in offer.reviews" :key="idx">{{review}}</span>
+      <br />
       <button class="btn-book">Book to LevelUp!</button>
     </div>
   </section>
@@ -18,6 +28,11 @@
 <script>
 export default {
   name: "OfferDetails",
+  data() {
+    return {
+      offer: null
+    };
+  },
   async created() {
     const offerId = this.$route.params._id;
     if (offerId) {
@@ -26,14 +41,15 @@ export default {
           type: "getOfferById",
           offerId
         });
+        this.getCurrOffer;
       } catch (err) {
         console.log(err);
       }
     }
   },
   computed: {
-    offer() {
-      return this.$store.getters.getCurrOffer;
+    getCurrOffer() {
+      this.offer = this.$store.getters.getCurrOffer;
     }
   }
 };
--- frontend/src/components/ProfileContent.vue
+++ frontend/src/components/ProfileContent.vue
@@ -1,13 +1,21 @@
 <template>
+  <section class="profile-content-container">
+      <button></button>
   <div class="profile-content">
+    
     <offer-details :currOffer="getCurrOffer"/>
   </div>
+  </section>
 </template>
 
 <script>
 import OfferDetails from "../components/OfferDetails";
 export default {
   name: "ProfileContent",
+  created() {
+          console.log('params', this.$route.params)
+        //   const offerId = this.$route.params._id;
+  },
   computed: {
     getCurrOffer() {
       return this.$store.getters.getCurrOffer

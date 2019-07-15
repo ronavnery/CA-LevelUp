d2b7ecc1b29bf771c0807ee4ca3d30455d34c9b4
Oriel Shalem <orielshalem@gmail.com>
2019-07-15 15:41:14 +0300

    add

---
+++ frontend/src/components/RequirementListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    {{ requirement }}
+      X
+    <!-- <button @click="$emit('remove', require.id)"> -->
+    <!-- </button> -->
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    requirement: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
